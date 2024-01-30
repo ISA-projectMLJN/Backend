7a21a2bf8a007216dd2c63cae071f001f5226f50
Jovana Ristic <jovana012002@gmail.com>
2024-01-30 18:05:27 +0100

    uzimanje opreme

--- Medicina/Controllers/ReservationController.cs
+++ Medicina/Controllers/ReservationController.cs
@@ -1,4 +1,5 @@
 ﻿using Medicina.Context;
+using Medicina.Migrations.Reservation;
 using Medicina.Models;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
@@ -41,22 +42,9 @@ namespace Medicina.Controllers
                 var app = _appointmentContext.Appointments.FirstOrDefault(u => u.ReservationId == res.Id);
                 if (appointments.Contains(app))
                 {
-                    if (!res.IsCollected && res.Deadline > DateTime.Now)
+                    if (!res.IsCollected)
                     {
                         uncollectedReservations.Add(res);
-                    }
-                    if(res.Deadline <= DateTime.Now)
-                    {
-                        var user = _userContext.Users.Find(res.UserId);
-                        user.PenaltyScore += 2;
-                        app.ReservationId = 0;
-                        app.Status = AppointmentStatus.Available;
-                        _userContext.Update(user);
-                        _reservationContext.Remove(res);
-                        _appointmentContext.Update(app);
-                        _userContext.SaveChanges();
-                        _reservationContext.SaveChanges();
-                        _appointmentContext.SaveChanges();
                     }
                 }
             }
@@ -66,6 +54,47 @@ namespace Medicina.Controllers
             }
 
             return Ok(uncollectedReservations);
+        }
+
+        [HttpGet("DidReservationExpire/{reservationId}")]
+        public ActionResult<bool> DidReservationExpire(int reservationId)
+        {
+            var res = _reservationContext.Reservations.Find(reservationId);
+            var app = _appointmentContext.Appointments.FirstOrDefault(u => u.ReservationId == res.Id);
+            if (res.Deadline <= DateTime.Now)
+            {
+                var user = _userContext.Users.Find(res.UserId);
+                user.PenaltyScore += 2;
+                app.ReservationId = 0;
+                app.Status = AppointmentStatus.Available;
+                _userContext.Update(user);
+                _reservationContext.Remove(res);
+                _appointmentContext.Update(app);
+                _userContext.SaveChanges();
+                _reservationContext.SaveChanges();
+                _appointmentContext.SaveChanges();
+                return Ok(true);
+            }
+            else return Ok(false);
+        }
+
+
+        [HttpPatch("ReservationCollected/{reservationId}")]
+        public ActionResult<bool> ReservationCollected(int reservationId)
+        {
+            var res = _reservationContext.Reservations.Find(reservationId);
+            var app = _appointmentContext.Appointments.FirstOrDefault(u => u.ReservationId == res.Id);
+                app.Status = AppointmentStatus.Collected;
+                res.IsCollected = true;
+                var eq = _equipmentContext.Equipment.FirstOrDefault(e=> e.Id == res.EquipmentId);
+                eq.Count -= res.EquipmentCount;
+                _appointmentContext.Update(app);
+                _reservationContext.Update(res);
+                _equipmentContext.Update(eq);
+                _reservationContext.SaveChanges();
+                _appointmentContext.SaveChanges();
+                _equipmentContext.SaveChanges();
+                return Ok(true);
         }
     }
 }
